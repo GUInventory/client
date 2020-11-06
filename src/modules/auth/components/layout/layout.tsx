9223c5645733f39fb408acc20d45d734178095a5
balintkiraly <balint.kiraly@protonmail.com>
2020-11-06 17:26:23 +0100

    Add links to layout

--- src/modules/auth/components/layout/layout.tsx
+++ src/modules/auth/components/layout/layout.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import { Box } from '@chakra-ui/core'
+import { Box, Flex, Link } from '@chakra-ui/core'
+import NextLink from 'next/link'
 
 export const Layout = ({ children }) => (
-  <Box maxW={460} mt={8} mx="auto">
-    {children}
-  </Box>
+  <>
+    <Flex justifyContent="space-between" p={4}>
+      <Box fontWeight="bold">GUINVENTORY</Box>
+      <Box>
+        <NextLink href="/auth/login">
+          <Link m={4}>Login</Link>
+        </NextLink>
+        <NextLink href="/auth/register">
+          <Link m={4}>Register</Link>
+        </NextLink>
+      </Box>
+    </Flex>
+    <Box maxW={460} mt={8} mx="auto">
+      {children}
+    </Box>
+  </>
 )
