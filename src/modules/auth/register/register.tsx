26c16afd46bb40f0e287e77932b1357b3d709468
balintkiraly <balint.kiraly@protonmail.com>
2020-11-06 16:21:13 +0100

    Use register mutation

--- src/modules/auth/register/register.tsx
+++ src/modules/auth/register/register.tsx
@@ -8,38 +8,66 @@ import {
 } from '@chakra-ui/core'
 import React from 'react'
 import { Layout } from '../components'
+import { useForm } from 'react-hook-form'
+import { useRegisterMutation } from '../graphql/register/register.generated'
 
-export const Register = () => (
-  <Layout>
-    <Flex direction="column">
-      <Heading fontSize="lg" textAlign="center">
-        Register
-      </Heading>
+type Inputs = {
+  name: string,
+  email: string,
+  password: string,
+}
 
-      <FormControl mb={4}>
-        <FormLabel htmlFor="email">
-          E-mail address
-        </FormLabel>
-        <Input name="email" type="email" placeholder="john.doe@example.org" />
-      </FormControl>
+export const Register = () => {
+  const { register, handleSubmit, errors } = useForm<Inputs>()
+  const [registerMutation] = useRegisterMutation()
 
-      <FormControl mb={4}>
-        <FormLabel htmlFor="password">
-            Password
-        </FormLabel>
-        <Input name="password" type="password" placeholder="*******" />
-      </FormControl>
+  const onSubmit = async (inputData) => {
+    const { data: { register: { token } } } = await registerMutation({ variables: inputData })
+    console.log(token)
+  }
 
-      <FormControl mb={4}>
-        <FormLabel htmlFor="password_again">
-            Password again
-        </FormLabel>
-        <Input name="password_again" type="password" placeholder="*******" />
-      </FormControl>
+  return (
+    <Layout>
+      <Flex direction="column">
+        <Heading fontSize="lg" textAlign="center">
+          Register
+        </Heading>
 
-      <Button variantColor="blue" type="submit">
-        Register
-      </Button>
-    </Flex>
-  </Layout>
-)
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+          <FormControl mb={4}>
+            <FormLabel htmlFor="email">
+              Name
+            </FormLabel>
+            <Input name="name" type="text" ref={register} placeholder="John Doe" />
+          </FormControl>
+
+          <FormControl mb={4}>
+            <FormLabel htmlFor="email">
+              E-mail address
+            </FormLabel>
+            <Input name="email" type="email" ref={register} placeholder="john.doe@example.org" />
+          </FormControl>
+
+          <FormControl mb={4}>
+            <FormLabel htmlFor="password">
+                Password
+            </FormLabel>
+            <Input name="password" type="password" ref={register} placeholder="*******" />
+          </FormControl>
+
+          <FormControl mb={4}>
+            <FormLabel htmlFor="password_again">
+                Password again
+            </FormLabel>
+            <Input name="password_again" type="password" placeholder="*******" />
+          </FormControl>
+
+          <Button variantColor="blue" type="submit">
+            Register
+          </Button>
+        </form>
+      </Flex>
+    </Layout>
+  )
+}
