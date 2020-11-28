21b2121b907e743e963431e980e0b7e5cf09ef7b
balintkiraly <balint.kiraly@protonmail.com>
2020-11-28 22:54:28 +0100

    Add missing headings

--- src/modules/item/pages/new.tsx
+++ src/modules/item/pages/new.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input, Flex } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
@@ -72,6 +72,7 @@ export const NewItem = () => {
           },
         ]}
       />
+      <Heading>Create Item</Heading>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl mb={4}>
           <FormLabel htmlFor="name">Name</FormLabel>
