1ced255b1a13e3ceaddc698933612acb4c5c5b30
balintkiraly <balint.kiraly@protonmail.com>
2020-11-29 16:57:34 +0100

    Add new warehouse validation

--- src/modules/warehouse/pages/new.tsx
+++ src/modules/warehouse/pages/new.tsx
@@ -1,18 +1,35 @@
-import { Button, FormControl, FormLabel, Heading, Input, Flex } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Flex,
+  FormErrorMessage,
+  InputGroup,
+  InputRightAddon,
+} from '@chakra-ui/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import { useCreateWarehouseMutation } from '../graphql/create.generated'
 import { ListMyWarehousesDocument } from '../graphql/list.generated'
 import { Breadcrumb } from '@modules/core/components'
+import { createSchema } from '../validators'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 type Inputs = {
   name: string
-  color: string
+  sizeX: number
+  sizeY: number
+  sizeZ: number
 }
 
 export const NewWarehouse = () => {
-  const { register, handleSubmit, errors } = useForm<Inputs>()
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    resolver: yupResolver(createSchema),
+  })
   const router = useRouter()
   const [createWarehouseMutation, { loading }] = useCreateWarehouseMutation()
 
@@ -51,19 +68,42 @@ export const NewWarehouse = () => {
       />
       <Heading>Create warehouse</Heading>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl mb={4}>
+        <FormControl mb={4} isInvalid={!!errors.name}>
           <FormLabel htmlFor="name">Name</FormLabel>
           <Input name="name" type="text" ref={register} />
+          <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
         </FormControl>
 
-        <FormControl mb={4}>
-          <FormLabel htmlFor="image">Size</FormLabel>
-          <Flex align="center">
-            <Input name="sizeX" type="number" ref={register} />x
-            <Input name="sizeY" type="number" ref={register} />x
-            <Input name="sizeZ" type="number" ref={register} />
-          </Flex>
-        </FormControl>
+        <FormLabel htmlFor="image">Size</FormLabel>
+        <Flex mb={4}>
+          <FormControl isInvalid={!!errors.sizeX}>
+            <InputGroup>
+              <Input name="sizeX" type="number" ref={register} />
+              <InputRightAddon children="cm" />
+            </InputGroup>
+            <FormErrorMessage>{errors.sizeX?.message}</FormErrorMessage>
+          </FormControl>
+          <Box px={3} pt={2}>
+            X
+          </Box>
+          <FormControl isInvalid={!!errors.sizeY}>
+            <InputGroup>
+              <Input name="sizeY" type="number" ref={register} />
+              <InputRightAddon children="cm" />
+            </InputGroup>
+            <FormErrorMessage>{errors.sizeY?.message}</FormErrorMessage>
+          </FormControl>
+          <Box px={3} pt={2}>
+            X
+          </Box>
+          <FormControl isInvalid={!!errors.sizeZ}>
+            <InputGroup>
+              <Input name="sizeZ" type="number" ref={register} />
+              <InputRightAddon children="cm" />
+            </InputGroup>
+            <FormErrorMessage>{errors.sizeZ?.message}</FormErrorMessage>
+          </FormControl>
+        </Flex>
 
         <Button colorScheme="blue" type="submit" isLoading={loading}>
           Create
