f4ac240dc6d01c20e77eee311e1c79216f994ffe
balintkiraly <balint.kiraly@protonmail.com>
2020-11-29 19:21:04 +0100

    Remove link and use move cursor

--- src/modules/storage/components/item_container/item.tsx
+++ src/modules/storage/components/item_container/item.tsx
@@ -28,42 +28,41 @@ const Item = ({ connectDragSource, setActiveItem, activeItem, item, ratio }) => 
 
   return connectDragSource(
     <div>
-      <Link p={1}>
-        <Popover trigger="hover">
-          <PopoverTrigger>
-            <Box
-              ref={drag}
-              opacity={isDragging ? 0.5 : 1}
-              position="absolute"
-              w={`${ratio * item.size.x}px`}
-              h={`${ratio * item.size.y}px`}
-              top={`${ratio * item.position.x}px`}
-              left={`${ratio * item.position.y}px`}
-              borderWidth="1px"
-              borderColor="blue.200"
-              backgroundColor={activeItem == item.id ? 'blue.300' : 'blue.400'}
-              _hover={{ backgroundColor: 'blue.300' }}
-              onMouseEnter={() => setActiveItem(item.id)}
-              onMouseLeave={() => setActiveItem('')}
-            >
-              <Box>{isDragging && ' (and I am being dragged now)'}</Box>
-            </Box>
-          </PopoverTrigger>
-
-          <PopoverContent
-            border="0"
-            bg="gray.700"
-            color="white"
-            zIndex={4}
-            width="300px"
-            opacity={0.8}
+      <Popover trigger="hover">
+        <PopoverTrigger>
+          <Box
+            cursor="move"
+            ref={drag}
+            opacity={isDragging ? 0.2 : 1}
+            position="absolute"
+            w={`${ratio * item.size.x}px`}
+            h={`${ratio * item.size.y}px`}
+            top={`${ratio * item.position.x}px`}
+            left={`${ratio * item.position.y}px`}
+            borderWidth="1px"
+            borderColor="blue.200"
+            backgroundColor={activeItem == item.id ? 'blue.300' : 'blue.400'}
+            _hover={{ backgroundColor: 'blue.300' }}
+            onMouseEnter={() => setActiveItem(item.id)}
+            onMouseLeave={() => setActiveItem('')}
           >
-            <Box p={5}>
-              <Text fontWeight="bold">{item.name}</Text>
-            </Box>
-          </PopoverContent>
-        </Popover>
-      </Link>
+            <Box></Box>
+          </Box>
+        </PopoverTrigger>
+
+        <PopoverContent
+          border="0"
+          bg="gray.700"
+          color="white"
+          zIndex={4}
+          width="300px"
+          opacity={0.8}
+        >
+          <Box p={5}>
+            <Text fontWeight="bold">{item.name}</Text>
+          </Box>
+        </PopoverContent>
+      </Popover>
     </div>,
   )
 }
